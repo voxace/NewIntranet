62e6fd6df387ea5d52b0a7834f1e40f95c3edf56
voxace <dave@voxace.com>
2018-07-19 21:46:16 +1000

    CSS / Menu Tweaks

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,46 +1,35 @@
-function getUrlVars() {
-  var vars = {};
-  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-    vars[key] = value;
-  });
-  return vars;
-}
+var pathArray = window.location.pathname.split('/');
 
-var cat = getUrlVars()["cat"];
-
-switch(cat) {
-    case "wifi":
-        $("#wifi" ).trigger("click");
-        break;
-    case "byod-info":
-        $("#byod-info" ).trigger("click");
-        break;
-    case "subject-info":
-        $("#subject-info" ).trigger("click");
-        break;
-    case "subject-info-stg4":
-        $("#subject-info" ).trigger("click");
+switch(pathArray[1]) {
+  case "wifi":
+    $("#wifi" ).trigger("click");
+    break;
+  case "byod":
+    $("#byod-info" ).trigger("click");
+    break;
+  case "subject":
+    $("#subject-info" ).trigger("click");
+    switch(pathArray[2]) {
+      case "stage4":
         $("#subject-info-stg4" ).trigger("click");
         break;
-    case "subject-info-stg5":
-        $("#subject-info" ).trigger("click");
+      case "stage5":
         $("#subject-info-stg5" ).trigger("click");
         break;
-    case "subject-info-stg5-electives":
-        $("#subject-info" ).trigger("click");
-        $("#subject-info-stg5" ).trigger("click");
-        $("#subject-info-stg5-electives" ).trigger("click");
-        break;
-    case "subject-info-stg6":
-        $("#subject-info" ).trigger("click");
+      case "stage6":
         $("#subject-info-stg6" ).trigger("click");
         break;
-    case "rewards-system":
-        $("#rewards-system" ).trigger("click");
-        break;
-    default:
-        $('#home').next().css("background", "rgba(0,0,0,0.1)");
+    }
+    break;
+  case "assessment":
+    $("#assessment-schedules" ).trigger("click");
+    break;
+  case "pbl":
+    $("#rewards-system" ).trigger("click");
+    break;
+  default:
+    $('#home').next().css("background", "rgba(0,0,0,0.1)");
 }
 
-var page = getUrlVars()["page"];
+var page = window.location.pathname;
 $('a[href*="' + page + '"]').parent().css("background", "rgba(0,0,0,0.1)" );
